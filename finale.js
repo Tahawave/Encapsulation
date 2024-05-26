d556fb9606ceaa783df954c6640697c1b00c628f
Tahawave <167723648+Tahawave@users.noreply.github.com>
2024-05-26 19:43:18 -0400

    Update finale.js

--- finale.js
+++ finale.js
@@ -5,6 +5,6 @@ alert('Wrong Password, Try Again');
 return false;
 }
 if (document.getElementById('Victory is ours').value == password) {
-alert('Correct, Encapsulation complete');
+alert('Correct, Encapsulation complete, nice job!');
 }
 }
